69e352cf1bbe7e1655b1e744e071a46ad23be06a
Nika Salamadze <nsalamadze97@gmail.com>
2020-03-14 18:14:42 -0400

    Basic header layout

--- app/javascript/components/Header.jsx
+++ app/javascript/components/Header.jsx
@@ -1,8 +1,28 @@
 import React from 'react';
+import CMACLogo1 from '../../assets/images/CMAC-logo-1.png';
+import CMACLogo2 from '../../assets/images/CMAC-logo-2.png';
+
 
 function Headeromponent() {
   return (
     <>
+      <div className="nav-menu-control-container">
+        <button type="button" aria-label="open-menu"><i className="fas fa-bars" /></button>
+      </div>
+      <div className="inner-header">
+        <div className="cmac-logo-container">
+          <img alt="logo-1" src={CMACLogo1} />
+        </div>
+        <div className="header-title-container">
+          <h1>Classical Martial Arts Center</h1>
+        </div>
+        <div className="cmac-logo-container">
+          <img alt="logo-1" src={CMACLogo2} />
+        </div>
+      </div>
+      <div className="nav-menu-control-container">
+        <button type="button" aria-label="open-menu"><i className="fas fa-sign-in-alt" /></button>
+      </div>
     </>
   );
 }
