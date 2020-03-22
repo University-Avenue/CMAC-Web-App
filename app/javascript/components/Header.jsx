4877a36e8a31dc3c6bdf1c82daebc803f9aa398b
Veraj <veraj.paruthi@gmail.com>
2020-03-22 18:07:39 -0400

    :construction: Added HTML and CSS for nav bar Still have yet to add the JS for it Deleted some out dated components

--- app/javascript/components/Header.jsx
+++ app/javascript/components/Header.jsx
@@ -1,12 +1,31 @@
 import React from 'react';
 import CMACLogo1 from '../../assets/images/CMAC-logo-1.png';
-import CMACLogo2 from '../../assets/images/CMAC-logo-2.png';
-import IconButton from './IconButton';
 
 function HeaderComponent() {
   return (
-    <>
-    </>
+    // <!-- Navigation-->
+    <nav className="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
+      <div className="container">
+        <a className="navbar-brand js-scroll-trigger" href="#page-top">
+          <img
+            src={CMACLogo1}
+            height={50}
+            width={50}
+            alt="CMAC Logo"
+          />
+          Classical Martial Arts Centre
+        </a>
+        <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon" /></button>
+        <div className="collapse navbar-collapse" id="navbarResponsive">
+          <ul className="navbar-nav ml-auto my-2 my-lg-0">
+            <li className="nav-item"><a className="nav-link js-scroll-trigger" href="#about">About</a></li>
+            <li className="nav-item"><a className="nav-link js-scroll-trigger" href="#services">Services</a></li>
+            <li className="nav-item"><a className="nav-link js-scroll-trigger" href="#portfolio">Portfolio</a></li>
+            <li className="nav-item"><a className="nav-link js-scroll-trigger" href="#contact">Contact</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   );
 }
 
