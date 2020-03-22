97bd98d26e4e23ecc7e229ebe2d6c076b6e690dc
Veraj <veraj.paruthi@gmail.com>
2020-03-22 19:27:38 -0400

    :sparkles: Navbar changes state based on user scroll position Added some color constants to css

--- app/javascript/components/Header.jsx
+++ app/javascript/components/Header.jsx
@@ -1,14 +1,30 @@
-import React, { useEffect } from 'react';
-import $ from 'jquery';
+import React, { useState, useEffect } from 'react';
 import CMACLogo1 from '../../assets/images/CMAC-logo-1.png';
 
+
+function listenToScroll(setScrolled) {
+  const winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  const scrollPercent = winScroll / height;
+
+  if (scrollPercent > 0.05) {
+    setScrolled('navbar-scrolled');
+  } else {
+    setScrolled('');
+  }
+}
+
 function HeaderComponent() {
+  const [scrolled, setScrolled] = useState('');
+
   useEffect(() => {
+    window.addEventListener('scroll', () => listenToScroll(setScrolled));
     console.log('useEffect()');
   }, []);
+
   return (
     // <!-- Navigation-->
-    <nav className="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
+    <nav className={`navbar navbar-expand-lg navbar-light fixed-top py-3 ${scrolled}`} id="mainNav">
       <div className="container">
         <a className="navbar-brand js-scroll-trigger" href="#page-top">
           <img
@@ -24,16 +40,16 @@ function HeaderComponent() {
         <div className="collapse navbar-collapse" id="navbarResponsive">
           <ul className="navbar-nav ml-auto my-2 my-lg-0">
             <li className="nav-item">
-              <a className="nav-link js-scroll-trigger" href="#Train">Train</a>
+              <a className="nav-link js-scroll-trigger" href="/train">Train</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link js-scroll-trigger" href="#Schedule">Schedule</a>
+              <a className="nav-link js-scroll-trigger" href="/schedule">Schedule</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link js-scroll-trigger" href="#contractus">Contact us</a>
+              <a className="nav-link js-scroll-trigger" href="/contractus">Contact us</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link js-scroll-trigger" href="#profile">
+              <a className="nav-link js-scroll-trigger" href="/profile">
                 <i className="fas fa-user" />
               </a>
             </li>
