83f6087a2bd56c95403b5e81c0374678e597340d
Veraj <veraj.paruthi@gmail.com>
2020-03-22 18:38:12 -0400

    :construction: Added useEffect hook Some CSS chnages

--- app/javascript/components/Header.jsx
+++ app/javascript/components/Header.jsx
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import $ from 'jquery';
 import CMACLogo1 from '../../assets/images/CMAC-logo-1.png';
 
 function HeaderComponent() {
+  useEffect(() => {
+    console.log('useEffect()');
+  }, []);
   return (
     // <!-- Navigation-->
     <nav className="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
@@ -12,16 +16,27 @@ function HeaderComponent() {
             height={50}
             width={50}
             alt="CMAC Logo"
+            className="navbar-logo"
           />
           Classical Martial Arts Centre
         </a>
         <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon" /></button>
         <div className="collapse navbar-collapse" id="navbarResponsive">
           <ul className="navbar-nav ml-auto my-2 my-lg-0">
-            <li className="nav-item"><a className="nav-link js-scroll-trigger" href="#about">About</a></li>
-            <li className="nav-item"><a className="nav-link js-scroll-trigger" href="#services">Services</a></li>
-            <li className="nav-item"><a className="nav-link js-scroll-trigger" href="#portfolio">Portfolio</a></li>
-            <li className="nav-item"><a className="nav-link js-scroll-trigger" href="#contact">Contact</a></li>
+            <li className="nav-item">
+              <a className="nav-link js-scroll-trigger" href="#Train">Train</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link js-scroll-trigger" href="#Schedule">Schedule</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link js-scroll-trigger" href="#contractus">Contact us</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link js-scroll-trigger" href="#profile">
+                <i className="fas fa-user" />
+              </a>
+            </li>
           </ul>
         </div>
       </div>
