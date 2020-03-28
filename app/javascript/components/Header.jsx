f04908bca5c9fc4d256eea8333eb5d368683a0f4
Veraj <veraj.paruthi@gmail.com>
2020-03-28 00:56:06 -0400

    :bug: Fixed issue with dropdown in nav bar Corrected jquery install Fixed issues in application.js

--- app/javascript/components/Header.jsx
+++ app/javascript/components/Header.jsx
@@ -22,65 +22,41 @@ function HeaderComponent() {
   }, []);
 
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <a className="navbar-brand" href="#">Navbar</a>
-      <button className="navbar-toggler" type="button" data-toggle="collapse"
-              data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02"
-              aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
-
-      <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-        <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
-          <li className="nav-item active">
-            <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+    <nav className={`navbar navbar-expand-lg navbar-light fixed-top py-3 ${scrolled}`} id="mainNav">
+      <a className="navbar-brand js-scroll-trigger" href="/">
+        <img
+          src={CMACLogo2}
+          height={50}
+          width={50}
+          alt="CMAC Logo"
+          className="navbar-logo"
+        />
+        CMAC
+      </a>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon" /></button>
+      <div className="collapse navbar-collapse" id="navbarResponsive">
+        <ul className="navbar-nav ml-auto my-2 my-lg-0">
+          <li className="nav-item">
+            <a className="nav-link js-scroll-trigger" href="/">Home</a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#">Link</a>
+            <a className="nav-link js-scroll-trigger" href="/tragit in">Train</a>
           </li>
           <li className="nav-item">
-            <a className="nav-link disabled" href="#">Disabled</a>
+            <a className="nav-link js-scroll-trigger" href="/schedule">Schedule</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link js-scroll-trigger" href="/contact_us">Contact us</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link js-scroll-trigger" href="/profile">
+              <i className="fas fa-user" />
+            </a>
           </li>
         </ul>
       </div>
     </nav>
   );
-  // return (
-  //   <nav className={`navbar navbar-expand-lg navbar-light fixed-top py-3 ${scrolled}`} id="mainNav">
-  //     <a className="navbar-brand js-scroll-trigger" href="/">
-  //       <img
-  //         src={CMACLogo2}
-  //         height={50}
-  //         width={50}
-  //         alt="CMAC Logo"
-  //         className="navbar-logo"
-  //       />
-  //       CMAC
-  //     </a>
-  //     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon" /></button>
-  //     <div className="collapse navbar-collapse" id="navbarResponsive">
-  //       <ul className="navbar-nav ml-auto my-2 my-lg-0">
-  //         <li className="nav-item">
-  //           <a className="nav-link js-scroll-trigger" href="/">Home</a>
-  //         </li>
-  //         <li className="nav-item">
-  //           <a className="nav-link js-scroll-trigger" href="/train">Train</a>
-  //         </li>
-  //         <li className="nav-item">
-  //           <a className="nav-link js-scroll-trigger" href="/schedule">Schedule</a>
-  //         </li>
-  //         <li className="nav-item">
-  //           <a className="nav-link js-scroll-trigger" href="/contact_us">Contact us</a>
-  //         </li>
-  //         <li className="nav-item">
-  //           <a className="nav-link js-scroll-trigger" href="/profile">
-  //             <i className="fas fa-user" />
-  //           </a>
-  //         </li>
-  //       </ul>
-  //     </div>
-  //   </nav>
-  // );
 }
 
 export default HeaderComponent;
