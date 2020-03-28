3c671aae79b8ea16c5bda71eb979e095c5d82522
Veraj <veraj.paruthi@gmail.com>
2020-03-27 20:01:39 -0400

    :construction: Added removeEventListener Started working on cleaning up scss

--- app/javascript/components/Header.jsx
+++ app/javascript/components/Header.jsx
@@ -1,44 +1,45 @@
 import React, { useState, useEffect } from 'react';
-import CMACLogo1 from '../../assets/images/CMAC-logo-1.png';
-
-
-function listenToScroll(setScrolled) {
-  const winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-  const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-  const scrollPercent = winScroll / height;
-
-  if (scrollPercent > 0.05) {
-    setScrolled('navbar-scrolled');
-  } else {
-    setScrolled('');
-  }
-}
+import CMACLogo2 from '../../assets/images/CMAC-logo-2.png';
 
 function HeaderComponent() {
   const [scrolled, setScrolled] = useState('');
 
+  const listenToScroll = () => {
+    const winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    const scrollPercent = winScroll / height;
+
+    if (scrollPercent > 0.05) {
+      setScrolled('navbar-scrolled');
+    } else {
+      setScrolled('');
+    }
+  };
+
   useEffect(() => {
-    window.addEventListener('scroll', () => listenToScroll(setScrolled));
-    console.log('useEffect()');
+    window.addEventListener('scroll', listenToScroll);
+    return () => window.removeEventListener('scroll', listenToScroll);
   }, []);
 
   return (
-    // <!-- Navigation-->
     <nav className={`navbar navbar-expand-lg navbar-light fixed-top py-3 ${scrolled}`} id="mainNav">
       <div className="container">
-        <a className="navbar-brand js-scroll-trigger" href="#page-top">
+        <a className="navbar-brand js-scroll-trigger" href="/">
           <img
-            src={CMACLogo1}
+            src={CMACLogo2}
             height={50}
             width={50}
             alt="CMAC Logo"
             className="navbar-logo"
           />
-          Classical Martial Arts Centre
+          CMAC
         </a>
         <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon" /></button>
         <div className="collapse navbar-collapse" id="navbarResponsive">
           <ul className="navbar-nav ml-auto my-2 my-lg-0">
+            <li className="nav-item">
+              <a className="nav-link js-scroll-trigger" href="/">Home</a>
+            </li>
             <li className="nav-item">
               <a className="nav-link js-scroll-trigger" href="/train">Train</a>
             </li>
