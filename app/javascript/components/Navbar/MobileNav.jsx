5faf24fed77478db0a86c99ef845127f11b52944
Veraj <veraj.paruthi@gmail.com>
2020-04-12 00:34:38 -0400

    :construction: Added transition for navbar

--- app/javascript/components/Navbar/MobileNav.jsx
+++ app/javascript/components/Navbar/MobileNav.jsx
@@ -1,21 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 function MobileNav() {
+  const [body, setBody] = useState(null);
+  const [content, setContent] = useState(null);
+
+  useEffect(() => {
+    setBody(document.getElementById('page-body'));
+    setContent(document.getElementById('page-content'));
+  }, []);
+
+  const toggleNav = () => {
+    body.classList.toggle('navigation');
+    content.classList.toggle('transition');
+  };
+
   return (
     <div className="mobile-container">
-      <div id="burgerBtn" />
+      <div className="banner">
+        <div id="burgerBtn" onClick={toggleNav} />
+      </div>
       <ul id="nav">
         <li>
           <a href="/">Home</a>
         </li>
         <li>
-          <a href="/">Home</a>
+          <a href="/">Train</a>
         </li>
         <li>
-          <a href="/">Home</a>
+          <a href="/">Schedule</a>
         </li>
         <li>
-          <a href="/">Home</a>
+          <a href="/">Contact Us</a>
         </li>
       </ul>
     </div>
