73add99e8b030a40a92c438a1bf1d5af5999f756
Veraj <veraj.paruthi@gmail.com>
2020-04-18 23:18:19 -0400

    :sparkles: Completed icon based nav

--- app/javascript/components/Navbar/MobileNav.jsx
+++ app/javascript/components/Navbar/MobileNav.jsx
@@ -1,39 +1,29 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
 function MobileNav() {
-  const [body, setBody] = useState(null);
-  const [content, setContent] = useState(null);
-
-  useEffect(() => {
-    setBody(document.getElementById('page-body'));
-    setContent(document.getElementById('page-content'));
-  }, []);
-
-  const toggleNav = () => {
-    body.classList.toggle('navigation');
-    content.classList.toggle('transition');
-  };
-
   return (
-    <div className="mobile-container">
-      <div className="banner">
-        <div id="burgerBtn" onClick={toggleNav} />
-      </div>
-      <ul id="nav">
-        <li>
-          <a href="/">Home</a>
-        </li>
-        <li>
-          <a href="/">Train</a>
-        </li>
-        <li>
-          <a href="/">Schedule</a>
-        </li>
-        <li>
-          <a href="/">Contact Us</a>
-        </li>
-      </ul>
-    </div>
+    <ul className="mobile-container">
+      <li>
+        <a href="/">
+          <i className="fas fa-home" />
+        </a>
+      </li>
+      <li>
+        <a href="/">
+          <i className="fas fa-heartbeat" />
+        </a>
+      </li>
+      <li>
+        <a href="/">
+          <i className="fas fa-calendar-day" />
+        </a>
+      </li>
+      <li>
+        <a href="/">
+          <i className="fas fa-phone" />
+        </a>
+      </li>
+    </ul>
   );
 }
 
