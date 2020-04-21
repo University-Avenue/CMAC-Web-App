168e1cb93a06c70145ee3b292525931254777d37
Veraj <veraj.paruthi@gmail.com>
2020-04-21 00:39:03 -0400

    :bug: Fixed navbar not showing

--- app/javascript/components/Navbar/DesktopNav.jsx
+++ app/javascript/components/Navbar/DesktopNav.jsx
@@ -35,7 +35,6 @@ function DesktopNav() {
         />
         CMAC
       </a>
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon" /></button>
       <div className="collapse navbar-collapse" id="navbarResponsive">
         <ul className="navbar-nav ml-auto my-2 my-lg-0">
           <li className="nav-item">
