7378781606d5c0ee5b1ad5520d38899bea2ad98e
Nika Salamadze <nsalamadze97@gmail.com>
2020-04-26 19:01:19 -0400

    :rocket: importing stylesheets from node_modules now works

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,8 +4,6 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import '../stylesheets/application.scss';
-
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
@@ -14,6 +12,7 @@ require("jquery")
 require("popper.js")
 require("bootstrap")
 
+import '../stylesheets/application.scss';
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,33 +1,4 @@
 /* eslint-disable */
-
-module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-          // Creates `style` nodes from JS strings
-          'style-loader',
-          // Translates CSS into CommonJS
-          'css-loader',
-          // Compiles Sass to CSS
-          'sass-loader',
-        ],
-      },
-    ],
-  },
-};
-
 const { environment } = require('@rails/webpacker')
-const webpack = require('webpack');
-
-environment.plugins.append(
-  'Provide',
-  new webpack.ProvidePlugin({
-    $: 'jquery',
-    jQuery: 'jquery',
-    Popper: ['popper.js', 'default']
-  })
-)
 
 module.exports = environment
