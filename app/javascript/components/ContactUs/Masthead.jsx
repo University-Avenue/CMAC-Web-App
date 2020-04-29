85236916b4e933514880e47ee52ecf00e95df298
Veraj <veraj.paruthi@gmail.com>
2020-04-29 00:20:55 -0400

    :construction: Started work on contact us page

--- app/javascript/components/ContactUs/Masthead.jsx
+++ app/javascript/components/ContactUs/Masthead.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import headerImage from '../../../assets/images/contact_us_header.jpg';
 
 const Masthead = () => (
   <header className="contact-masthead">
     <div className="container">
+      <img src={headerImage} alt="header" className="header-img" />
       <div className="row align-items-center text-center">
         <div className="col-md-12">
           <h1>Contact Us</h1>
