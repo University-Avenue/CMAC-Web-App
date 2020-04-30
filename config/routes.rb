3c3d265a8645f243a5108e8ebe943bf400d5cc55
Veraj <veraj.paruthi@gmail.com>
2020-04-30 01:16:43 -0400

    :construction: Set up contact us form

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :contacts, only:[:new, :create]
   get 'contact_us' => 'pages#contact_us'
   get 'schedule' => 'pages#schedule'
   get 'profile' => 'pages#profile'
