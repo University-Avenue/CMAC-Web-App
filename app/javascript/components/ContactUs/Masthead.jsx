62025ab5e93bf3cede9d59797631ac6fc207f923
Veraj <veraj.paruthi@gmail.com>
2020-04-30 00:31:40 -0400

    :contruction: Deleted contact us mailer

--- app/javascript/components/ContactUs/Masthead.jsx
+++ app/javascript/components/ContactUs/Masthead.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
-import headerImage from '../../../assets/images/contact_us_header.jpg';
 
 const Masthead = () => (
   <header className="contact-masthead">
     <div className="container">
-      <img src={headerImage} alt="header" className="header-img" />
       <div className="row align-items-center text-center">
         <div className="col-md-12">
-          <h1>Contact Us</h1>
+          <h1 className="text-light">Contact Us</h1>
         </div>
       </div>
     </div>
