004970f06041e08baf5762a78c9a037e65ec3131
Veraj <veraj.paruthi@gmail.com>
2020-05-08 00:07:25 -0400

    :art: Completed Contact Us Page Finished implementing success and error messages

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,15 +1,13 @@
 class ContactsController < ApplicationController
-  def new
-    @contact = Contact.new
-  end
   def create
     @contact = Contact.new(params[:contact])
     @contact.request = request
     if @contact.deliver
-      flash.now[:error] = nil
+      flash[:success] = "Thank you for your message! We'll get back to you soon!"
+      redirect_to contact_us_url
     else
-      flash.now[:error] = 'Cannot send message.'
-      render :new
+      flash[:error] = 'Oops something went wrong. Please try again.'
+      redirect_to contact_us_url
     end
   end
 end
