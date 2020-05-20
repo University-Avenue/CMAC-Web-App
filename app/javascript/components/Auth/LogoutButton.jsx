1806eeace83721c07e7e2380347dadec9ca90f74
Veraj Paruthi <veraj.paruthi@gmail.com>
2020-05-19 21:36:13 -0400

    :bug: Fixed log out button bug

--- app/javascript/components/Auth/LogoutButton.jsx
+++ app/javascript/components/Auth/LogoutButton.jsx
@@ -4,13 +4,19 @@ import axios from 'axios';
 export default function LogoutButton() {
   const logout = () => {
     axios.delete('http://localhost:3000/logout', { withCredentials: true })
-      .then((response) => { console.log(response); })
+      .then((response) => {
+        if (response.status === 200) {
+          window.location.href = '/';
+        }
+        console.log(response);
+      })
       .catch((error) => { console.log(error); });
   };
   return (
-
-    <a className="nav-link" href="/" onSubmit={logout}>
-      <i className="fas fa-sign-in-alt" />
-    </a>
+    <button type="submit" className="logout-button nav-link" onClick={logout}>
+      <i
+        className="fas fa-sign-out-alt"
+      />
+    </button>
   );
 }
