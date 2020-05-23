5e5d521b3b2248e4045b00c41a2e8e50feb7000e
Nika Salamadze <nsalamadze97@gmail.com>
2020-05-23 18:34:43 -0400

    :lipstick: Added am actual 'our story' closes #48

--- app/javascript/components/Home/OurStory.jsx
+++ app/javascript/components/Home/OurStory.jsx
@@ -15,11 +15,10 @@ export default function OurStorySection() {
             <img src={CMACLogo2} alt="" />
           </div>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-            labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-            nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit
-            esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt
-            in culpa qui officia deserunt mollit anim id est laborum.
+            We offer a traditional style in a modern way. Students learn self-defense through a
+            seriesof drills and forms that strengthen both the body and mind, promoting confidence
+            and goodhealth.  The benefits to training in a classical art are truly unlimited -
+            which students and parents often notice even after a short while of practice.
           </p>
         </div>
       </div>
